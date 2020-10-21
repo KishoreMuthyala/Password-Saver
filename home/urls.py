5d11e8ccafc59e1410605b2ef6bed582a8605b90
KishoreMuthyala <67886934+KishoreMuthyala@users.noreply.github.com>
2020-10-21 18:38:16 +0530

    added extra features

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name="home"),
     path('<int:id>', views.delete, name="delete"),
-    path('update/<int:id>', views.update, name="update"),
+    path('update/<int:id>/<str:val>', views.update, name="update"),
+    path('search/<int:id>', views.search, name="search"),
 ]
--- home/views.py
+++ home/views.py
@@ -6,19 +6,22 @@ from .models import Passwords
 
 
 def home(request):
-    if request.method == 'POST':
-        website_url = request.POST["website"]
-        website_name = request.POST["website_name"]
-        email = request.POST["Email_User"]
-        password = request.POST["password"]
-        user_id = request.POST['user_id']
-        # print(user_id)
-        website = Passwords(website_url=website_url,
-                            website_name=website_name, email=email, password=password, user_id=user_id)
-        website.save()
-    user_id = request.user.id
-    websites = Passwords.objects.filter(user_id=user_id)
-    return render(request, 'home.html', {'websites': websites, })
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            website_url = request.POST["website"]
+            website_name = request.POST["website_name"]
+            email = request.POST["Email_User"]
+            password = request.POST["password"]
+            user_id = request.POST['user_id']
+            # print(user_id)
+            website = Passwords(website_url=website_url,
+                                website_name=website_name, email=email, password=password, user_id=user_id)
+            website.save()
+        user_id = request.user.id
+        websites = Passwords.objects.filter(user_id=user_id)
+        return render(request, 'home.html', {'websites': websites, })
+    else:
+        return redirect("login")
 
 
 def delete(request, id):
@@ -26,19 +29,35 @@ def delete(request, id):
     return redirect('home')
 
 
-def update(request, id):
+def update(request, id, val):
     pwd = Passwords.objects.get(id=id)
     if request.method == 'POST':
-        website_url = request.POST["website_url"]
-        website_name = request.POST["website_name1"]
-        email = request.POST["Email_User1"]
-        password = request.POST["password1"]
-        pwd.website_url = website_url
-        pwd.website_name = website_name
-        pwd.email = email
-        pwd.password = password
-        pwd.save()
-        return redirect("home")
+        if val != 'form':
+            website_url = request.POST["website_url"]
+            website_name = request.POST["website_name1"]
+            email = request.POST["Email_User1"]
+            password = request.POST["password1"]
+            pwd.website_url = website_url
+            pwd.website_name = website_name
+            pwd.email = email
+            pwd.password = password
+            pwd.save()
+            return redirect("home")
+        return render(request, 'update.html', {
+            'pas': pwd
+        })
     return render(request, 'update.html', {
         'pas': pwd
     })
+
+
+def search(request, id):
+    if request.method == 'POST':
+        qu = request.POST["web"]
+        webs = []
+        webs += (list(Passwords.objects.filter(website_name__icontains=qu, user_id=id)))
+        webs += (list(Passwords.objects.filter(website_url__icontains=qu, user_id=id)))
+        webs += (list(Passwords.objects.filter(email__icontains=qu, user_id=id)))
+
+        return render(request, 'home.html', {'websites': webs})
+    return render(request, 'home.html')
