a6e2f5e61802607e4cae55059565c84d8260561b
KishoreMuthyala <67886934+KishoreMuthyala@users.noreply.github.com>
2020-11-01 15:36:16 +0530

    deploy

--- password_saver/settings.py
+++ password_saver/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n(&rt!no!kj@bet)3*!693y0g@1v@c)cs7txkg2(lg1gqze^lu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['password-saver-kishore.herokuapp.com', 'localhost']
 
